2be8ca27489bb497de6c13d9e8c19c5891e479d6
GabrielDev <gabrys.wisniewski@op.pl>
2024-01-23 14:16:44 +0100

    fix: value has never read

--- client/src/hooks/usedashboardHeaderLogic.ts
+++ client/src/hooks/usedashboardHeaderLogic.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect, useContext } from "react";
+import {  useContext } from "react";
 import { isMobileContext, themeContext } from "../components/pages/dashboardPage";
 
 export const useDashboardHeaderLogic = (props: {
