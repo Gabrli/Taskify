2f77cde8b61722ba572867b8fd5a3f43b48dafcb
GabrielDev <gabrys.wisniewski@op.pl>
2024-01-27 15:12:35 +0100

    fix: text in button

--- client/src/components/landingPageComponents/heroComponents/contentComponents/buttonHowItWorking.tsx
+++ client/src/components/landingPageComponents/heroComponents/contentComponents/buttonHowItWorking.tsx
@@ -4,6 +4,6 @@ import { themeContext } from "../../../../App"
 export default function ButtonHowItWorking(){
     const theme = useContext(themeContext)
     return (
-        <a href="/#process" className={`btn_howIt transition duration-700 ease-in-out pl-7  text-base font-medium pr-7 pt-3 relative  top-0 mr-auto  pb-3   ${theme === 'dark'? "text-text_dark" : "text-text_light"}   mobile1:left-0 mobile1:top-0  mobile1:mr-auto mobile1:w-full `}><a href="#">How it working ?</a></a>
+        <a href="/#process" className={`btn_howIt transition duration-700 ease-in-out pl-7  text-base font-medium pr-7 pt-3 relative  top-0 mr-auto  pb-3   ${theme === 'dark'? "text-text_dark" : "text-text_light"}   mobile1:left-0 mobile1:top-0  mobile1:mr-auto mobile1:w-full mobile1:text-center`}><a href="#">How it working ?</a></a>
     )
 }
