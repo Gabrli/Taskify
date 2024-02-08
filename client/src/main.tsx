28fc37d7ce19ed9e68f8ec63f340c0d5a2b50a2a
GabrielDev <gabrys.wisniewski@op.pl>
2024-02-08 18:46:06 +0100

    add: sitemap.xml file

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+
 import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
    <React.StrictMode>
-    <App />
+    
+      <App />
+  
   </React.StrictMode>
   </BrowserRouter>
 )
